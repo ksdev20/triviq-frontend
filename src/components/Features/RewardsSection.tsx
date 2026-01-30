84afd83c71bde975e90f96f5028f6b4c28bdb597
ksdev20 <batbale01@gmail.com>
2026-01-30 20:49:22 +0530

    reused FeaturesSection for RewardsSection, fully responsive

--- src/components/Features/RewardsSection.tsx
+++ src/components/Features/RewardsSection.tsx
@@ -1,28 +1,28 @@
-import { featuresSection } from "../../config/components/home-page";
+import { rewardsSection } from "../../config/components/home-page";
 import { FeatureCard } from "./FeatureCard";
 import { TopTextSection } from "./TopTextSection";
 import "../../styles/components/features-section.css";
 
-export function FeaturesSection() {
+export function RewardsSection() {
   return (
-    <section className="features-section consistent-padding-x">
-      <TopTextSection textObj={featuresSection.textObj} />
+    <section className="features-section rewards consistent-padding-x">
+      <TopTextSection textObj={rewardsSection.textObj} />
       <div className="features-cards">
         <div className="g1">
           <FeatureCard
-            obj={featuresSection.featureCards[0]}
+            obj={rewardsSection.featureCards[0]}
             key={0}
             variant={"img-right"}
           />
         </div>
         <div className="g2">
           <FeatureCard
-            obj={featuresSection.featureCards[1]}
+            obj={rewardsSection.featureCards[1]}
             key={1}
             variant={"default"}
           />
           <FeatureCard
-            obj={featuresSection.featureCards[2]}
+            obj={rewardsSection.featureCards[2]}
             key={2}
             variant={"default"}
           />
--- src/config/components/home-page.ts
+++ src/config/components/home-page.ts
@@ -1,8 +1,8 @@
 export const featuresSection = {
   textObj: {
     top: "Features",
-    head: "WHY PLAYERS CHOOSE US",
-    subtitle: "Join thousands competing daily",
+    head: "Built for real competition",
+    subtitle: "Play instantly, anywhere, anytime",
   },
   featureCards: [
     {
@@ -24,6 +24,40 @@ export const featuresSection = {
       textObj: {
         head: "Thousands of questions",
         subtitle: "Explore diverse categories and endless topics",
+      },
+      img: "https://cdn.pixabay.com/photo/2025/11/28/14/40/sea-9983074_1280.jpg",
+    },
+  ],
+};
+
+export const rewardsSection = {
+  textObj: {
+    top: "Rewards",
+    head: "Why players choose us",
+    subtitle: "Join thousands competing daily",
+  },
+  featureCards: [
+    {
+      textObj: {
+        top: "Social",
+        head: "Challenge your friends",
+        subtitle: "Send invites and settle debates",
+      },
+      img: "https://cdn.pixabay.com/photo/2025/11/28/14/40/sea-9983074_1280.jpg",
+    },
+    {
+      textObj: {
+        top: "Global",
+        head: "Compete worldwide",
+        subtitle: "Test yourself against skilled players",
+      },
+      img: "https://cdn.pixabay.com/photo/2025/11/28/14/40/sea-9983074_1280.jpg",
+    },
+    {
+      textObj: {
+        top: "Growth",
+        head: "Expand your mind",
+        subtitle: "Learn while you play and climb ranks",
       },
       img: "https://cdn.pixabay.com/photo/2025/11/28/14/40/sea-9983074_1280.jpg",
     },
