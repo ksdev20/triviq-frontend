7fe0decd73ca0c21e9200f618414cd347224333a
ksdev20 <batbale01@gmail.com>
2026-01-31 18:11:23 +0530

    added netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  adapter: netlify(),
   output: "server",
   devToolbar: {
     enabled: false,
   },
   vite: {
-        plugins: [tailwindcss()],
-    }
+    plugins: [tailwindcss()],
+  },
 });
